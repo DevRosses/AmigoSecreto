c3041f7abb1d228deb7c4ada13f6ce53b55ce291
devrosses <devrosses@gmail.com>
2025-02-05 00:01:35 -0300

    Implementa una función para sortear los amigos y verifica que no se repitan los nombres ingresados

--- app.js
+++ app.js
@@ -14,14 +14,36 @@ function listaAmigos() {
   }
 }
 
+function encontrarPosicion(lista, elemento) {
+  return lista.indexOf(elemento);
+}
+
 function agregarAmigo() {
   let amigo = document.getElementById("amigo").value;
 
+  let encontrado = encontrarPosicion(amigos, amigo);
+
+  console.log(`encontrado: ${encontrado}`);
+
   if (amigo === "") {
-    alert("Por favor, inserte un nombre.");
+    alert("Por favor, ingresa un nombre.");
+  } else if (encontrado >= 0) {
+    alert("Ese amigo ya esta jugando!, ingresa otro nombre.");
   } else {
     amigos.push(amigo);
     listaAmigos();
   }
   limpiarInput();
+}
+
+function sortearAmigo() {
+  let indiceRandom = Math.floor(Math.random() * amigos.length);
+
+  let seleccionado = amigos[indiceRandom];
+
+  console.log(`Amigo Seleccionado: ${seleccionado}`);
+
+  let resultado = document.getElementById("resultado");
+
+  resultado.innerHTML = `<li> ${seleccionado} </li>`;
 }
